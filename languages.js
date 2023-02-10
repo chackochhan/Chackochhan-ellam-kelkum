25c866c71d3c48fc10fbbbbc9dffa4d5d1a65595
Chacko G George <100925554+chackochhan@users.noreply.github.com>
2023-02-10 09:59:10 +0530

    First commit
    Basic changes in the CSS and HTML files being made. JS script code is unchanged

---
+++ languages.js
@@ -0,0 +1,29 @@
+const languages = [
+  {
+    no: "16",
+    name: "English",
+    native: "English",
+    code: "en",
+  },
+
+  {
+    no: "11",
+    name: "Chinese (Traditional)",
+    native: "中文繁體",
+    code: "zh-TW",
+  },
+  
+  {
+    no: "33",
+    name: "Japanese",
+    native: "日本語",
+    code: "ja",
+  },
+
+ {
+    no: "27",
+    name: "Hindi",
+    native: "हिन्दी",
+    code: "hi",
+  },
+];
